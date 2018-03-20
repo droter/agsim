9c1e04bd033bd00264cf5fbee893c2460c5eaf8d
Matt Droter <matt@droter.com>
2018-03-20 18:16:23 -0500

    Update ros-entrypoint.sh

--- ros-entrypoint.sh
+++ ros-entrypoint.sh
@@ -3,4 +3,7 @@
 source /opt/ros/kinetic/setup.bashsource /etc/bash.bashrc
 source ~/agsim_ws/devel/setup.bash
 source /etc/bash.bashrc
+date >> ~/agsim_ws/logs/roscore.log
+roscore & >> ~/agsim_ws/logs/roscore.log 2>&1
+sleep 5
 exec "$@"
