b337e84c9948dd8c321d36e12cce58701b48d199
Matt Droter <matt@droter.com>
2018-03-22 11:57:51 -0500

    Update ros-entrypoint.sh

--- ros-entrypoint.sh
+++ ros-entrypoint.sh
@@ -3,8 +3,16 @@
 source /opt/ros/kinetic/setup.bash
 source ~/agsim_ws/devel/setup.bash
 source ~/.bashrc
-source /etc/bash.bashrc
 date >> ~/agsim_ws/logs/roscore.log
-roscore & >> ~/agsim_ws/logs/roscore.log 2>&1
+
+result='ps aux | grep -i "roscore" | grep -v "grep" | wc -l'
+if [ $result -ge 1 ]
+	then
+		echo "roscore is running..."
+	else
+		echo "starting roscore...."
+    roscore & >> ~/agsim_ws/logs/roscore.log 2>&1
+fi
 sleep 5
+
 exec "$@"
