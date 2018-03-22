034f8897babb94bebad48e900e864f4dcd0af7ab
Matt Droter <matt@droter.com>
2018-03-22 17:09:21 +0000

    Updated permissions

--- ros-entrypoint.sh
+++ ros-entrypoint.sh
@@ -3,15 +3,17 @@
 source /opt/ros/kinetic/setup.bash
 source ~/agsim_ws/devel/setup.bash
 source ~/.bashrc
-date >> ~/agsim_ws/logs/roscore.log
 
+# Start roscore 
+date >> ~/agsim_ws/logs/roscore.log
 result='ps aux | grep -i "roscore" | grep -v "grep" | wc -l'
 if [ $result -ge 1 ]
 	then
 		echo "roscore is running..."
 	else
 		echo "starting roscore...."
-    roscore & >> ~/agsim_ws/logs/roscore.log 2>&1
+		date >> ~/agsim_ws/logs/roscore.log
+    		roscore & >> ~/agsim_ws/logs/roscore.log 2>&1
 fi
 sleep 5
 
